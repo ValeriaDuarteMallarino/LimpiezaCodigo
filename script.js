25bd205691778e0879a77502b5177bd00c3a9e25
Valeria Duarte <valdm0505@gmail.com>
2023-09-13 21:24:12 -0300

    jiji

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var boton = document.getElementById("btn");
-var box = document.getElementById("caja");
+const botton = document.getElementById("btn");
+const box = document.getElementById("caja");
 
-function CambiarColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
+
+//cambia de color las letras 
+function changeColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     caja.style.backgroundColor = color;
 }
 
-boton.addEventListener("click", CambiarColor);
+botton.addEventListener("click", changeColor);
